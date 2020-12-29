8ce82458480944f2d384ab0fefff1f971d2346e0
RomanPodymov <podymfrombryansk@yandex.ru>
2020-12-29 22:16:41 +0100

    Eureka and Localizable.strings

--- CoMameDobreho/OfferScreen.swift
+++ CoMameDobreho/OfferScreen.swift
@@ -6,11 +6,29 @@
 //  Copyright © 2020 CoMameDobreho. All rights reserved.
 //
 
-import UIKit
+import Eureka
+import Foundation
 
-final class OfferScreen: UIViewController {
+final class OfferScreen: FormViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        form +++ Section(NSLocalizedString("offer_screen.title", comment: ""))
+            <<< TextRow { row in
+                row.title = NSLocalizedString("offer_screen.soup", comment: "")
+                row.placeholder = NSLocalizedString("offer_screen.text_placeholder", comment: "")
+            }
+            <<< TextRow { row in
+                row.title = NSLocalizedString("offer_screen.first_meal", comment: "")
+                row.placeholder = NSLocalizedString("offer_screen.text_placeholder", comment: "")
+            }
+            <<< TextRow { row in
+                row.title = NSLocalizedString("offer_screen.second_meal", comment: "")
+                row.placeholder = NSLocalizedString("offer_screen.text_placeholder", comment: "")
+            }
+            <<< TextRow { row in
+                row.title = NSLocalizedString("offer_screen.third_meal", comment: "")
+                row.placeholder = NSLocalizedString("offer_screen.text_placeholder", comment: "")
+            }
 
         PeripheralManager.shared.start()
     }
