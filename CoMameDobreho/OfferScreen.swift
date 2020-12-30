14a52b20a412935231e843fb9c4a691a6ade8f52
RomanPodymov <podymfrombryansk@yandex.ru>
2020-12-30 11:28:18 +0100

    Advertising data for all rows

--- CoMameDobreho/OfferScreen.swift
+++ CoMameDobreho/OfferScreen.swift
@@ -9,27 +9,51 @@
 import Eureka
 import Foundation
 
+enum OfferScreenTag: String {
+    case soup
+    case firstMeal
+    case secondMeal
+    case thirdMeal
+}
+
 final class OfferScreen: FormViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         form +++ Section(NSLocalizedString("offer_screen.title", comment: ""))
-            <<< TextRow { row in
+            <<< TextRow(OfferScreenTag.soup.rawValue) { row in
                 row.title = NSLocalizedString("offer_screen.soup", comment: "")
                 row.placeholder = NSLocalizedString("offer_screen.text_placeholder", comment: "")
             }
-            <<< TextRow { row in
+            <<< TextRow(OfferScreenTag.firstMeal.rawValue) { row in
                 row.title = NSLocalizedString("offer_screen.first_meal", comment: "")
                 row.placeholder = NSLocalizedString("offer_screen.text_placeholder", comment: "")
             }
-            <<< TextRow { row in
+            <<< TextRow(OfferScreenTag.secondMeal.rawValue) { row in
                 row.title = NSLocalizedString("offer_screen.second_meal", comment: "")
                 row.placeholder = NSLocalizedString("offer_screen.text_placeholder", comment: "")
             }
-            <<< TextRow { row in
+            <<< TextRow(OfferScreenTag.thirdMeal.rawValue) { row in
                 row.title = NSLocalizedString("offer_screen.third_meal", comment: "")
                 row.placeholder = NSLocalizedString("offer_screen.text_placeholder", comment: "")
             }
+            <<< ButtonRow { row in
+                row.title = NSLocalizedString("offer_screen.button_save", comment: "")
+                row.onCellSelection { [weak self] _, _ in
+                    self?.onSave()
+                }
+            }
+    }
 
-        PeripheralManager.shared.start()
+    private func onSave() {
+        let offerScreenTagsAndValues: [(OfferScreenTag, String)] = form.values().compactMap {
+            guard let offerScreenTag = OfferScreenTag(rawValue: $0) else {
+                return nil
+            }
+            guard let value = $1 as? String else {
+                return nil
+            }
+            return (offerScreenTag, value)
+        }
+        PeripheralManager.shared.start(with: .init(uniqueKeysWithValues: offerScreenTagsAndValues))
     }
 }
--- CoMameDobreho/PeripheralManager.swift
+++ CoMameDobreho/PeripheralManager.swift
@@ -8,17 +8,39 @@
 
 import CoreBluetooth
 
+private enum CharacteristicId: String, CaseIterable {
+    case soup = "b1faa5b2-95b1-436c-9bc5-82815228a3e1"
+    case firstMeal = "b1faa5b2-95b1-436c-9bc5-82815228a3e2"
+    case secondMeal = "b1faa5b2-95b1-436c-9bc5-82815228a3e3"
+    case thirdMeal = "b1faa5b2-95b1-436c-9bc5-82815228a3e4"
+
+    var asOfferScreenTag: OfferScreenTag {
+        switch self {
+        case .soup:
+            return .soup
+        case .firstMeal:
+            return .firstMeal
+        case .secondMeal:
+            return .secondMeal
+        case .thirdMeal:
+            return .thirdMeal
+        }
+    }
+}
+
 final class PeripheralManager: NSObject {
     static let shared = PeripheralManager()
 
     private var peripheralManager: CBPeripheralManager!
 
-    private static let mainServiceId = CBUUID(string: "f64642dc-22f5-450f-a2db-a0ab07c3d47f")
-    private static let mainCharacteristicId = CBUUID(string: "b1faa5b2-95b1-436c-9bc5-82815228a3e1")
+    private static let mainServiceId = CBUUID(
+        string: "f64642dc-22f5-450f-a2db-a0ab07c3d47f"
+    )
 
-    private let dataString = "Hello"
+    private var data: [OfferScreenTag: String]?
 
-    public final func start() {
+    public final func start(with data: [OfferScreenTag: String]) {
+        self.data = data
         peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
     }
 }
@@ -34,15 +56,16 @@ extension PeripheralManager: CBPeripheralManagerDelegate {
     }
 
     func setupServices() {
-        let valueData = dataString.data(using: .utf8)
-        let mainCharacteristic = CBMutableCharacteristic(
-            type: Self.mainCharacteristicId,
-            properties: [.read],
-            value: valueData,
-            permissions: [.readable]
-        )
+        let mainCharacteristics = CharacteristicId.allCases.map {
+            CBMutableCharacteristic(
+                type: CBUUID(string: $0.rawValue),
+                properties: [.read],
+                value: self.data?[$0.asOfferScreenTag].flatMap { $0.data(using: .utf8) },
+                permissions: [.readable]
+            )
+        }
         let mainService = CBMutableService(type: Self.mainServiceId, primary: true)
-        mainService.characteristics = [mainCharacteristic]
+        mainService.characteristics = mainCharacteristics
         peripheralManager.add(mainService)
         startAdvertising()
     }
